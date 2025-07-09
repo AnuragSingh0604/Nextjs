f2b7ef246f83ff401f6bea4930a86a99a5de745a
Anurag Singh <anurag2307singh@gmail.com>
2025-07-09 13:23:24 +0530

    Updated code inside submodule

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,6 +1,27 @@
 import { comments } from "./data" ;
-export async function GET(){
-  return Response.json(comments);
+import {type NextRequest} from "next/server";
+import {headers,cookies} from "next/headers";
+export async function GET(request:NextRequest){
+  //const requestHeaders=new Headers(request.headers);
+  //const auth= requestHeaders.get("Authorization");
+  const headersList=await headers();
+  console.log(headersList.get("Authorization"));
+  const searchParams=request.nextUrl.searchParams;
+  const query=searchParams.get("query");
+  const filteredComments=query?comments.filter((comment)=>comment.text.includes(query)):comments;
+  //getting cookies from nextRequest
+  const theme=request.cookies.get("theme");
+  // getting cookies from cookies method
+  const cookieStore=await cookies();
+  cookieStore.set("resultPerPage","20");
+  console.log(cookieStore.get("resultsPerPage")); 
+  return Response.json(filteredComments);
+  return new Response("<h1>profile api data</>",{
+    headers:{
+      "Content-Type":"text/html",
+      "Set-Cookie":"theme=dark",
+    }
+  });
 }
 export async function POST(request:Request){
   const comment=await request.json();
